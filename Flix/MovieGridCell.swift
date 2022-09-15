2db58098c42befe5731ef4b55a7af616908bfbae
Camila Aichele <103790405+camila1014@users.noreply.github.com>
2022-09-15 13:29:19 -0400

    Add files via upload

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Camila Aichele on 9/14/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
